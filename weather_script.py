e8a1cf556dc081a450244ba51e0c645a82992d17
triple-s-rank <specialforwar@icloud.com>
2022-05-25 12:44:27 +0300

    weather script refactored

--- weather_script.py
+++ weather_script.py
@@ -1,17 +1,15 @@
 import requests
 
-wttr_sheremetyevo = 'https://wttr.in/Шереметьево'
-wttr_london = 'https://wttr.in/Лондон'
-wttr_cherepovets = 'https://wttr.in/Череповец'
+places_list = [
+	'https://wttr.in/Шереметьево',
+	'https://wttr.in/Лондон',
+	'https://wttr.in/Череповец',
+]
 
 params = {'lang': 'ru', 'n': '', 'T': '', 'q': ''}
 
-weather_response_list = [
-	requests.get(wttr_london, params=params),
-	requests.get(wttr_cherepovets, params=params),
-	requests.get(wttr_sheremetyevo, params=params),
-]
+for place in places_list:
+	print(requests.get(place, params=params).text)
 
-for response in weather_response_list:
-	print(response.text)
+
 
