8d91cb1d789e4b3e8a1c075e93f3c0942b0a18a7
triple-s-rank <specialforwar@icloud.com>
2022-05-30 00:18:40 +0300

    simplifyed code by making constant WTTR URL

--- weather_script.py
+++ weather_script.py
@@ -2,17 +2,18 @@ import requests
 
 WTTR_URL = 'https://wttr.in/'
 
-def main():
-	params = {'lang': 'ru', 'n': '', 'T': '', 'q': '', 'm': ''}
-	places = ['Шереметьево', 'Лондон', 'Череповец']
+params = {'lang': 'ru', 'n': '', 'T': '', 'q': '', 'm': ''}
+map_points = ['Шереметьево', 'Лондон', 'Череповец']
+
+def main(places):
 	try:
 		for place in places:
 			url = WTTR_URL + place
 			response = requests.get(url, params=params)
 			response.raise_for_status()
-			return (response.text)
+			print(response.text)
 	except requests.exceptions.HTTPError as error:
 		return('Http error', error)
 
 if __name__ == '__main__':
-	print(main())
+	main(map_points)
