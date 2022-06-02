60a261efa4777eef870377419f970c0becd6c238
triple-s-rank <specialforwar@icloud.com>
2022-06-02 21:08:10 +0300

    clear function + docstring + type annotation

--- weather_script.py
+++ weather_script.py
@@ -5,15 +5,21 @@ WTTR_URL = 'https://wttr.in/'
 params = {'lang': 'ru', 'n': '', 'T': '', 'q': '', 'm': ''}
 map_points = ['Шереметьево', 'Лондон', 'Череповец']
 
-def main(places):
+def main(places: list) -> list:
+
+	""" Returns list of request.Response objects with weather of places passed as arguments """
+
+	responses = []
 	try:
 		for place in places:
 			url = WTTR_URL + place
 			response = requests.get(url, params=params)
 			response.raise_for_status()
-			print(response.text)
+			responses.append(response)
+		return responses
 	except requests.exceptions.HTTPError as error:
 		print('Http error', error)
 
 if __name__ == '__main__':
-	main(map_points)
+	for response in main(map_points):
+		print(response.text)
